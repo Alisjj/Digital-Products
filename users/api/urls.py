3d0d0f339586a88bc4133b0b4ad5a1d356116ff3
Alisjj <aliyussajjads@gmail.com>
2022-02-06 21:07:55 +0100

    middleware and referesh token

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from dj_rest_auth.registration.views import VerifyEmailView, ConfirmEmailView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
     path('registration/', include('dj_rest_auth.registration.urls')),
+    path('token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('verify-email', VerifyEmailView.as_view(), name='account_email_verification_sent'),
-    
-    # path('products/', include('products.api.urls')),
 ]
