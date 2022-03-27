f989967a4aec3b2d32a4ea878f2bace374186643
Alisjj <aliyussajjads@gmail.com>
2022-03-27 23:48:52 +0100

    login receiver 2

--- subscription/models.py
+++ subscription/models.py
@@ -2,7 +2,6 @@ import os
 from rave_python import Rave
 from django.db import models
 from django.db.models.signals import post_save
-from django.contrib.auth.signals import user_logged_in
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
@@ -31,13 +30,4 @@ def post_save_user(sender, instance, created, *args, **kwargs):
         free = Pricing.objects.get(name="Free")
         Subscription.objects.create(user=instance, pricing=free)
 
-def user_logged_in_receiver(sender, user, request, **kwargs):
-    subscription = user.subscription
-    print(subscription)
-    if subscription.pricing.name != "Free":
-        sub = rave.Subscriptions.fetch(subscription.flw_subscription_id)
-        subscription.status = sub['returnedData']['data']['plansubsptions'][0]['status']
-
-
-user_logged_in.connect(user_logged_in_receiver)
 post_save.connect(post_save_user, sender=User)
